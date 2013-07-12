bdfd075f4e8713da803c874907c6b73b26164f2c
Ubuntu <ubuntu@ip-172-31-28-14.us-west-2.compute.internal>
2013-07-12 13:43:15 +0000

    update grader 2nd

--- grader.js
+++ grader.js
@@ -61,7 +61,7 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
-issuef(require.main == module) {
+if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
